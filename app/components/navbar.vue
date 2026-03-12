bdad4e5305c503f9ef5ed46cd2073ed19a10b76d
Lestine-Yan <lestine0056@qq.com>
2026-03-12 16:52:19 +0800

    更新一个简单的主页雏形和导航

---
+++ app/components/navbar.vue
@@ -0,0 +1,16 @@
+<!-- components/Navbar.vue -->
+<template>
+  <header class="py-3 z-50 fixed inset-x-0 animate-slide-down">
+    <div class="container mx-auto flex justify-center">
+      <nav class="bg-white/40 backdrop-blur-lg rounded-full px-10 py-3 shadow-lg border border-white/30">
+        <ul class="flex space-x-8 text-center">
+          <li><NuxtLink to="/" class="px-6 py-2 rounded-full font-medium transition-all duration-300 hover:bg-red-700 hover:text-white">首页</NuxtLink></li>
+          <li><NuxtLink to="/talk" class="px-6 py-2 rounded-full font-medium transition-all duration-300 hover:bg-red-700 hover:text-white">杂谈</NuxtLink></li>
+          <li><NuxtLink to="/learn" class="px-6 py-2 rounded-full font-medium transition-all duration-300 hover:bg-red-700 hover:text-white">随笔</NuxtLink></li>
+          <li><NuxtLink to="/myfriends" class="px-6 py-2 rounded-full font-medium transition-all duration-300 hover:bg-red-700 hover:text-white">友人帐</NuxtLink></li>
+          <li><NuxtLink to="/about" class="px-6 py-2 rounded-full font-medium transition-all duration-300 hover:bg-red-700 hover:text-white">关于我</NuxtLink></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
