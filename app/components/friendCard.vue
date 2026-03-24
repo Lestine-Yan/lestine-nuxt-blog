3618342fdc4fdf7bc3714cd230d4dac2b3b62b20
Lestine-Yan <lestine0056@qq.com>
2026-03-24 19:40:31 +0800

    新增friend友链卡片

---
+++ app/components/friendCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <a :href="url" target="_blank">
+    <div class=" bg-white/50 p-6 rounded-md  w-[200px] h-[200px] m-4 border border-white shadow-sm">
+        <img :src="image" alt="友链" class="w-24 h-24 rounded-full mx-auto">
+        <h2 class="text-lg font-bold my-1">{{ name }}</h2>
+        <p class="text-sm text-gray-500">{{ description }}</p>
+    </div>
+    </a>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps({
+    url: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    name: {
+        type: String,
+        default: ''
+    },
+    description: {
+        type: String,
+        default: ''
+    },
+    
+})
+</script>
