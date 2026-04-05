a558c8d02a699316599b94865ca75e6c309f063f
Lestine <ilestine@outlook.com>
2026-04-06 00:16:21 +0800

    修复bg宽度显示问题 (#14)
    1.修复bg模糊效果在手机端出现宽度并未完全贴合视窗的问题
    2.修复友链卡片宽度超出视窗的问题

--- app/components/friendCard.vue
+++ app/components/friendCard.vue
@@ -1,6 +1,6 @@
 <template>
     <a :href="url" target="_blank">
-    <div class=" bg-white/50 hover:bg-white/70 shadow-md hover:shadow-xl p-6 rounded-md  w-[400px] md:w-[200px] h-[200px] m-4 border border-white transition-all duration-300 animate-fade-up">
+    <div class=" bg-white/50 hover:bg-white/70 shadow-md hover:shadow-xl p-6 rounded-md  w-[280px] md:w-[200px] h-[200px] m-4 border border-white transition-all duration-300 animate-fade-up">
         <img :src="image" alt="友链" class="w-24 h-24 rounded-full mx-auto">
         <h2 class="text-lg font-bold my-1">{{ name }}</h2>
         <p class="text-sm text-gray-500">{{ description }}</p>
--- app/pages/myfriends/index.vue
+++ app/pages/myfriends/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full-no-scrollbar min-h-[100dvh] bg-white/70 z-10 flex flex-col  py-24 backdrop-blur-lg">
+    <div class="w-[100dvw] md:w-full-no-scrollbar min-h-[100dvh] bg-white/70 z-10 flex flex-col  py-24 backdrop-blur-lg">
         <!--占位符，待完善-->
         <h1 class="text-4xl font-bold text-center py-16">友人帐</h1>
         <div class="md:flex mx-auto">
