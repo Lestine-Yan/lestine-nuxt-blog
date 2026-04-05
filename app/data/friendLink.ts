7ff1492c10433ed4541867d5c2610361924a3eb6
Lestine-Yan <lestine0056@qq.com>
2026-04-05 15:00:44 +0800

    优化dockerfile提升构建速度

--- app/data/friendLink.ts
+++ app/data/friendLink.ts
@@ -4,13 +4,13 @@ export const friendLink = [
         name: '糖糖球',
         description: '嘻嘻嘻是会长大人',
         url: 'https://tantanchugasuki.cn/',
-        image: '/images/friends/1.jpg'
+        image: 'https://img.tantanchugasuki.cn/i/r/avatar'
     },
     {
         id: 2,
         name: 'Piter',
         description: 'ai大佬awa',
         url: 'https://npiter.de/',
-        image: '/images/friends/2.jpg'
+        image: 'https://upload-bbs.miyoushe.com/upload/2025/01/13/363839390/3b27ef57435c87542b43e4fecdabc040_3189688091243815374.png?x-oss-process=image//resize,s_600/quality,q_80/auto-orient,0/interlace,1/format,png'
     }
 ]
