840b72864982502a3b751ec5946aa3281818cace
Lestine-Yan <lestine0056@qq.com>
2026-04-20 21:41:10 +0800

    优化404界面UI

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -15,8 +15,10 @@ const { data: page } = await useAsyncData(
     <article class="w-full px-4 sm:px-6 md:px-8 lg:px-12 flex flex-col mx-auto animate-fade-up transition-all duration-300 max-w-3xl xl:max-w-4xl 2xl:max-w-4xl">
       <header></header>
       <ContentRenderer v-if="page" :value="page" />
-      <div v-else class="flex items-center justify-center min-h-[50dvh]">
-        <p class="text-gray-500">页面未找到</p>
+      <div v-else class="mx-auto items-center justify-center flex flex-col min-h-[50dvh] ">
+        <img src="/images/svg/404.svg" alt="404" class="my-4 object-contain" />
+        <p class="text-red-700 text-2xl font-bold mt-4">未找到页面</p>
+        <NuxtLink to="/" class="bg-red-700 hover:bg-red-400 hover:shadow-md px-8 py-2 mt-8 rounded-xl text-white  text-2xl font-bold  transition-all duration-300 ">回到首页</NuxtLink>
       </div>
       <footer></footer>
     </article>
